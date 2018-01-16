1196f6b4ea9565bd9a91d00c0f81c6bc3f611f10
Brooke <nel13012@byui.edu>
2018-01-16 16:58:30 -0700

    adding new hello

---
+++ web/hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World</title>
+</head>
+<body style="background-color:powderblue;">
+<p>
+	<?php 
+		echo 'Hello from PHP!';	
+	?>
+</p>
+</body>
+</html>
