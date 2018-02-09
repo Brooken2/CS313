2ee2e69431cbc8bab24680f1ed1d74012926bf06
Brooke <nel13012@byui.edu>
2018-02-09 16:38:56 -0700

    adding game rock paper scissors

---
+++ web/gamerps.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Game Rock-Paper-Scissors</title>
+	<link rel="stylesheet" 
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+	</script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+	</script>
+	<link rel="stylesheet" type="text/css" href="shopping.css">
+</head>
+
+<body>
+
+<?php
+
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+?>
+</body>
+</html>
---
+++ web/projectGameSQL.sql
@@ -0,0 +1,39 @@
+
+CREATE DATABASE userGame;
+
+CREATE TABLE public.participant
+(
+	id SERIAL NOT NULL PRIMARY KEY,
+	username VARCHAR(100) NOT NULL UNIQUE,
+	password VARCHAR(100) NOT NULL,
+	display_name VARCHAR(100) NOT NULL,
+	email VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE public.game
+(
+	id SERIAL NOT NULL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE public.gameParticipants
+(
+	participantId INT NOT NULL REFERENCES public.participant(Id),
+	gameId INT NOT NULL REFERENCES public.game(Id),
+	totalGames INT NOT NULL,
+	wins INT NOT NULL
+);
+
+INSERT INTO participant(username, password, display_name, email)
+		VALUES
+		('Brooken2', '220546Bn', 'Brooke Nelson', 'brookenelson220546@gmail.com');
+
+INSERT INTO game(name) VALUES ('Rock, Paper, Sissors');
+
+INSERT INTO gameParticipants(participantId, gameId, totalGames, wins)
+VALUES (0, 0, 10, 5);
+		
+connections
+INSERT INTO gameParticipants(participantId, gameId, totalGames, wins)
+		VALUES
+		(SELECT id FROM participant WHERE username = 'Brooken2')
