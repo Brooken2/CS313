9105c01eb9c19c545803e94456f2427b1dd3ed52
Brooke <nel13012@byui.edu>
2018-02-10 10:39:35 -0700

    adding changes to display

--- web/Project1/home.php
+++ web/Project1/home.php
@@ -16,6 +16,8 @@
   <div class="page-header">
 		<h1 class="display-3">Welcome To The Game!</h1>    
   </div>
+
+
 <form action="gamerps.php">
   <div class="form-group">
     <label for="login">User Name:</label>
@@ -25,22 +27,12 @@
     <label for="pwd">Password:</label>
     <input type="password" class="form-control" id="pwd">
   </div>
-  <button type="submit" class="btn btn-default">Submit</button>
+  <button type="submit" class="btn btn-default">
+  		<span class="glyphicon glyphicon-log-in"></span> Login</button>
+ <button type="subimit" class="btn btn-success btn-sm">
+ 		<span class="glyphicon glyphicon-log-out"></span> Log-Out</button>
 </form> 
 
-
-<form action="gamerps.php">
-  User Name:<br>
-  <input type="text" name="username">
-  <br>
-  Password:<br>
-  <input type="text" name="password">
-  <br><br>
-  <input type="submit" class="btn btn-success btn-sm"> 
-		<span class="glyphicon glyphicon-log-in"></span> Login</input>
-	<input type="submit" class="btn btn-success btn-sm">
-		<span class="glyphicon glyphicon-log-out"></span> Log-Out</input>
-</form>
 </div>
 
 </body>
