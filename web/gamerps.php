336d8f8d4a2db14909c6407286e3a129f5c2b9c2
Brooke <nel13012@byui.edu>
2018-02-09 18:17:59 -0700

    adding changes to display

--- web/gamerps.php
+++ web/gamerps.php
@@ -41,7 +41,7 @@ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass
 <div class="row">
 	<div class="column center">
 		<?php 
-		foreach ($db->query('SELECT display_name FROM  participant WHERE display_name = "Brooke Nelson" ') as $row)
+		foreach ($db->query("SELECT display_name FROM  participant WHERE display_name = 'Brooke Nelson' ") as $row)
 		{
   			echo 'Display Name: ' . $row['display_name'];
   			echo '<br/>';
