acdda932e92ba6a4f11b78aaf4965feb97b6e44b
Brooke <nel13012@byui.edu>
2018-02-10 10:34:21 -0700

    adding changes to display

--- web/Project1/home.php
+++ web/Project1/home.php
@@ -11,11 +11,23 @@
 	</script>
 </head>
 <body>
-<form action="gamerps.php">
+
 <div class="container">
   <div class="page-header">
 		<h1 class="display-3">Welcome To The Game!</h1>    
   </div>
+<form action="gamerps.php">
+  <div class="form-group">
+    <label for="login">User Name:</label>
+    <input type="login" class="form-control" id="login">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="pwd">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form> 
+
 
 <form action="gamerps.php">
   User Name:<br>
